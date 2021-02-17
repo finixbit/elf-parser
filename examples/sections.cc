0ec46c75c93d08867ffab6b6b4d046daeb20e348
Property404 <martinez.dagan@gmail.com>
2021-02-17 14:19:07 -0600

    Spaces->Tabs

--- examples/sections.cc
+++ examples/sections.cc
@@ -5,36 +5,36 @@
 void print_sections(const std::vector<elf_parser::section_t> &sections);
 
 int main(int argc, char* argv[]) {
-    const char* usage_banner = "usage: ./sections [<executable>]\n";
-    if(argc < 2) {
-        std::cerr << usage_banner;
-        return -1;
-    }
+	const char* usage_banner = "usage: ./sections [<executable>]\n";
+	if(argc < 2) {
+		std::cerr << usage_banner;
+		return -1;
+	}
 
-    const std::string program((std::string)argv[1]);
-    const elf_parser::Elf_parser elf_parser(program);
+	const std::string program((std::string)argv[1]);
+	const elf_parser::Elf_parser elf_parser(program);
 
-    const std::vector<elf_parser::section_t> secs = elf_parser.get_sections();
-    print_sections(secs);
-    return 0;
+	const std::vector<elf_parser::section_t> secs = elf_parser.get_sections();
+	print_sections(secs);
+	return 0;
 }
 
 void print_sections(const std::vector<elf_parser::section_t> &sections) {
-    printf("  [Nr] %-16s %-16s %-16s %s\n", "Name", "Type", "Address", "Offset");
-    printf("       %-16s %-16s %5s\n",
-                    "Size", "EntSize", "Align");
-    
-    for (const auto &section : sections) {
-        printf("  [%2d] %-16s %-16s %016" PRIx64 " %08" PRIx64 "\n", 
-            section.section_index,
-            section.section_name.c_str(),
-            section.section_type.c_str(),
-            section.section_addr, 
-            section.section_offset);
+	printf("  [Nr] %-16s %-16s %-16s %s\n", "Name", "Type", "Address", "Offset");
+	printf("       %-16s %-16s %5s\n",
+			"Size", "EntSize", "Align");
 
-        printf("       %016x %016" PRIx64 " %5" PRIu64 "\n",
-            section.section_size, 
-            static_cast<uint64_t>(section.section_ent_size),
-            static_cast<uint64_t>(section.section_addr_align));
-    }
+	for (const auto &section : sections) {
+		printf("  [%2d] %-16s %-16s %016" PRIx64 " %08" PRIx64 "\n", 
+				section.section_index,
+				section.section_name.c_str(),
+				section.section_type.c_str(),
+				section.section_addr, 
+				section.section_offset);
+
+		printf("       %016x %016" PRIx64 " %5" PRIu64 "\n",
+				section.section_size, 
+				static_cast<uint64_t>(section.section_ent_size),
+				static_cast<uint64_t>(section.section_addr_align));
+	}
 }
--- examples/segments.cc
+++ examples/segments.cc
@@ -5,35 +5,35 @@
 void print_segments(const std::vector<elf_parser::segment_t> &segments);
 
 int main(int argc, char* argv[]) {
-    char usage_banner[] = "usage: ./sections [<executable>]\n";
-    if(argc < 2) {
-        std::cerr << usage_banner;
-        return -1;
-    }
+	char usage_banner[] = "usage: ./sections [<executable>]\n";
+	if(argc < 2) {
+		std::cerr << usage_banner;
+		return -1;
+	}
 
-    const std::string program((std::string)argv[1]);
-    const elf_parser::Elf_parser elf_parser(program);
+	const std::string program((std::string)argv[1]);
+	const elf_parser::Elf_parser elf_parser(program);
 
-    const std::vector<elf_parser::segment_t> segs = elf_parser.get_segments();
-    print_segments(segs);
-    return 0;
+	const std::vector<elf_parser::segment_t> segs = elf_parser.get_segments();
+	print_segments(segs);
+	return 0;
 }
 
 void print_segments(const std::vector<elf_parser::segment_t> &segments) {
-    printf("  %-16s  %-16s   %-16s   %s\n", "Type", "Offset", "VirtAddr", "PhysAddr");
-    printf("  %-16s  %-16s   %-16s  %6s %5s\n", " ", "FileSiz", "MemSiz", "Flags", "Align");
+	printf("  %-16s  %-16s   %-16s   %s\n", "Type", "Offset", "VirtAddr", "PhysAddr");
+	printf("  %-16s  %-16s   %-16s  %6s %5s\n", " ", "FileSiz", "MemSiz", "Flags", "Align");
 
-    for (const auto &segment : segments) {
-        printf("   %-16s 0x%016" PRIx64 " 0x%016" PRIx64 " 0x%016" PRIx64 "\n",
-                segment.segment_type.c_str(), 
-                segment.segment_offset,
-                segment.segment_virtaddr, 
-                segment.segment_physaddr);
-        
-        printf("   %-16s 0x%016" PRIx64 " 0x%016" PRIx64 " %-5s %-5" PRIx64 "\n", "",
-                segment.segment_filesize, 
-                segment.segment_memsize,
-                segment.segment_flags.c_str(), 
-                static_cast<uint64_t>(segment.segment_align));
-    }
+	for (const auto &segment : segments) {
+		printf("   %-16s 0x%016" PRIx64 " 0x%016" PRIx64 " 0x%016" PRIx64 "\n",
+				segment.segment_type.c_str(), 
+				segment.segment_offset,
+				segment.segment_virtaddr, 
+				segment.segment_physaddr);
+
+		printf("   %-16s 0x%016" PRIx64 " 0x%016" PRIx64 " %-5s %-5" PRIx64 "\n", "",
+				segment.segment_filesize, 
+				segment.segment_memsize,
+				segment.segment_flags.c_str(), 
+				static_cast<uint64_t>(segment.segment_align));
+	}
 }
